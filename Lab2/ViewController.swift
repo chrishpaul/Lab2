5f2d801faff1db91afb497050619027c6a42a1f1
chrishpaul <87882234+chrishpaul@users.noreply.github.com>
2024-09-29 16:47:19 -0700

    Initial Commit

---
+++ Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab2
+//
+//  Created by Chrishnika Paul on 9/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
